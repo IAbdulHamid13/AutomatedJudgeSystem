e3a4480151e43c377e1ae16182ea63ac857c29be
Isa Abdul-Hamid <isaabdulhamid13@gmail.com>
2024-10-29 17:32:02 -0400

    I accidentally left my path in App.java. I removed it.

--- src/main/java/org/example/Views/App.java
+++ src/main/java/org/example/Views/App.java
@@ -5,6 +5,7 @@ public class App {
         System.out.println("Hello World"); //keep this to make sure the project still runs.
         JavaZipFileTypeReader reader = new JavaZipFileTypeReader();
         ZipReader zr = new ZipReader(reader);
-        zr.parseFile("C:\\Users\\Isaab\\OneDrive\\Desktop\\comp3607groupproject\\readThis.zip");
+        String directory = new String("");
+        zr.parseFile(directory);
     }
 }
